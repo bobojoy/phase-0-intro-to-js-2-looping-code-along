b56bf1e5d096e803999eb5c08df9fd0ed7976b3b
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-07-03 23:19:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event ="surprise";
+
+function writeCards( names, events ) {
+    let personalCards =[]
+    for ( let i = 0; i < names.length; i++ ){
+        personalCards.push( 'Thank you, ${names[i]}, for the wonderful ${events} gift!')
+    }
+    return personalCards;
+}
+
+function  countDown() {
+   let numCountdown=11;
+   while (numCountdown--) {
+    console.log(numCountdown);
+   }
+}
+
+
+
+    
